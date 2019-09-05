9145e5118a7ab824220433643d6efc16e040c67c
beth billings <bethfbillings@gmail.com>
2019-09-05 14:36:37 -0500

    Added Enemy class and Mana.

--- Beast_Blitz/Beast_Blitz.Domain/Models/BattleStats.cs
+++ Beast_Blitz/Beast_Blitz.Domain/Models/BattleStats.cs
@@ -3,6 +3,7 @@ namespace Beast_Blitz.Domain.Models
     public class BattleStats
     {
         public int Health { get; set; }
+        public int Mana { get; set; }
         public int Attack { get; set; }
         public int Speed { get; set; }
         public int Defense { get; set; }
---
+++ Beast_Blitz/Beast_Blitz.Domain/Models/Enemy.cs
@@ -0,0 +1,7 @@
+namespace Beast_Blitz.Domain.Models
+{
+    public class Enemy : Pet
+    {
+      public decimal Multiplier { get; set; }
+    }
+}
