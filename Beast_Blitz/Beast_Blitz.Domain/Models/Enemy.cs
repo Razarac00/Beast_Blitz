3163704569a569a6dce14017682828c86e19cfb5
beth billings <bethfbillings@gmail.com>
2019-09-05 14:58:04 -0500

    Added some functionality to Pet and Enemy

--- Beast_Blitz/Beast_Blitz.Domain/Models/Enemy.cs
+++ Beast_Blitz/Beast_Blitz.Domain/Models/Enemy.cs
@@ -3,5 +3,10 @@ namespace Beast_Blitz.Domain.Models
     public class Enemy : Pet
     {
       public decimal Multiplier { get; set; }
+
+      public Enemy(string NewName, string NewColor, Species NewSpecies) : base(NewName, NewColor, NewSpecies)
+      {
+
+      }
     }
 }
--- Beast_Blitz/Beast_Blitz.Domain/Models/Pet.cs
+++ Beast_Blitz/Beast_Blitz.Domain/Models/Pet.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace Beast_Blitz.Domain.Models
 {
     public class Pet
     {
       public string Name { get; set; }
       public int Age { get; set; }
+      public DateTime CreationDate { get; set; }
       public string Color { get; set; }
 
       public BattleStats BattleStats { get; set; }
@@ -13,5 +16,17 @@ namespace Beast_Blitz.Domain.Models
 
       public Hat Hat { get; set; }
       public Armor Armor { get; set; }
+
+      public Pet(string NewName, string NewColor, Species NewSpecies)
+      {
+        Name = NewName;
+        Age = 0;
+        CreationDate = DateTime.Now; 
+        Color = NewColor;
+        Species = NewSpecies;
+        BattleStats = Species.BaseBattleStats;
+        CareStats = Species.BaseCareStats;
+      }
+
     }
 }
