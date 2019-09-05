1e948c248f7d8319d5f4c292349a2c6c8b889177
Razarac00 <orji.john@outlook.com>
2019-09-05 15:41:56 -0500

    Added reference from domain to testing, added sellingprice to accessory, started hattesting

---
+++ Beast_Blitz/Beast_Blitz.Testing/HatTesting.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Beast_Blitz.Domain.Models;
+
+namespace Beast_Blitz.Testing
+{
+    public class HatTesting
+    {
+        [Fact]
+        public void CanBeConstructed()
+        {
+        //Given
+        var hat = new Hat();
+        //When
+        
+        //Then
+        }
+    }
+}
