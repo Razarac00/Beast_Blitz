96211f16845e33dfac7216c5376aa4f31d4f9d0d
beth billings <bethfbillings@gmail.com>
2019-09-09 15:56:09 -0500

    Restructured objects

--- Beast_Blitz/Beast_Blitz.Domain/Abstracts/Accessory.cs
+++
@@ -1,9 +0,0 @@
-namespace Beast_Blitz.Domain.Abstracts
-{
-    public abstract class Accessory
-    {
-      public string Name { get; set; } 
-      public int Price { get; set; }
-      public string Color { get; set; }
-    }
-}
