e5582ff817e431e5f1d5a2571bcf87b133c93a19
Razarac00 <orji.john@outlook.com>
2019-09-13 12:04:50 -0500

    Db sets without abstracts added to dbcontext

--- Beast_Blitz/Beast_Blitz.Data/Beast_Blitz_DbContext.cs
+++ Beast_Blitz/Beast_Blitz.Data/Beast_Blitz_DbContext.cs
@@ -1,3 +1,5 @@
+using Beast_Blitz.Domain.Abstracts;
+using Beast_Blitz.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Beast_Blitz.Data
@@ -5,27 +7,27 @@ namespace Beast_Blitz.Data
     public class Beast_Blitz_DbContext : DbContext
     {
         // public DbSet<User> Users { get; set; }
-        // public DbSet<Player> Players { get; set; }
-        // public DbSet<Admin> Admins { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Admin> Admins { get; set; }
         // public DbSet<Monster> Monsters { get; set; }
-        // public DbSet<Pet> Pets { get; set; }
-        // public DbSet<Enemy> Enemies { get; set; }
-        // public DbSet<Boss> Bosses { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+        public DbSet<Boss> Bosses { get; set; }
         // public DbSet<Location> Locations { get; set; }
-        // public DbSet<Shop> Shops { get; set; }
-        // public DbSet<Home> Homes { get; set; }
-        // public DbSet<Battlefield> Battlefields { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Home> Homes { get; set; }
+        public DbSet<Battlefield> Battlefields { get; set; }
         // public DbSet<Item> Items { get; set; }
-        // public DbSet<Treat> Treats { get; set; }
-        // public DbSet<Food> Foods { get; set; }
-        // public DbSet<Potion> Potions { get; set; }
+        public DbSet<Treat> Treats { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Potion> Potions { get; set; }
         // public DbSet<Accessory> Accessories { get; set; }
-        // public DbSet<Armor> Armors { get; set; }
-        // public DbSet<Hat> Hats { get; set; }
-        // public DbSet<Species> Species { get; set; }
-        // public DbSet<Element> Elements { get; set; }
-        // public DbSet<BattleStats> BattleStats { get; set; }
-        // public DbSet<CareStats> CareStats { get; set; }
+        public DbSet<Armor> Armors { get; set; }
+        public DbSet<Hat> Hats { get; set; }
+        public DbSet<Species> Species { get; set; }
+        public DbSet<Element> Elements { get; set; }
+        public DbSet<BattleStats> BattleStats { get; set; }
+        public DbSet<CareStats> CareStats { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
