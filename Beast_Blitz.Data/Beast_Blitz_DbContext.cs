2868cd5c7edd3e7c56cf49da24c5e31e083a8aa2
Razarac00 <orji.john@outlook.com>
2019-09-15 18:22:38 -0500

    commented out all dbsets that inherit from abstract

--- Beast_Blitz.Data/Beast_Blitz_DbContext.cs
+++ Beast_Blitz.Data/Beast_Blitz_DbContext.cs
@@ -14,19 +14,19 @@ namespace Beast_Blitz.Data
         public DbSet<Accessory> Accessories { get; set; }
 
         // CONCRETE //
-        public DbSet<Player> Players { get; set; }
-        public DbSet<Admin> Admins { get; set; }
-        public DbSet<Pet> Pets { get; set; }
-        public DbSet<Enemy> Enemies { get; set; }
-        public DbSet<Boss> Bosses { get; set; }
-        public DbSet<Shop> Shops { get; set; }
-        public DbSet<Home> Homes { get; set; }
-        public DbSet<Battlefield> Battlefields { get; set; }
-        public DbSet<Treat> Treats { get; set; }
-        public DbSet<Food> Foods { get; set; }
-        public DbSet<Potion> Potions { get; set; }
-        public DbSet<Armor> Armors { get; set; }
-        public DbSet<Hat> Hats { get; set; }
+        // public DbSet<Player> Players { get; set; }
+        // public DbSet<Admin> Admins { get; set; }
+        // public DbSet<Pet> Pets { get; set; }
+        // public DbSet<Enemy> Enemies { get; set; }
+        // public DbSet<Boss> Bosses { get; set; }
+        // public DbSet<Shop> Shops { get; set; }
+        // public DbSet<Home> Homes { get; set; }
+        // public DbSet<Battlefield> Battlefields { get; set; }
+        // public DbSet<Treat> Treats { get; set; }
+        // public DbSet<Food> Foods { get; set; }
+        // public DbSet<Potion> Potions { get; set; }
+        // public DbSet<Armor> Armors { get; set; }
+        // public DbSet<Hat> Hats { get; set; }
         public DbSet<Species> Species { get; set; }
         public DbSet<Element> Elements { get; set; }
         public DbSet<BattleStats> BattleStats { get; set; }
