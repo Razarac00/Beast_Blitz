b1bdd62bc463d1b8c3a6f73bc5c79ceffe4d89dd
Razarac00 <orji.john@outlook.com>
2019-09-16 13:11:34 -0500

    data annotations for location

--- Beast_Blitz.Domain/Abstracts/Location.cs
+++ Beast_Blitz.Domain/Abstracts/Location.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Beast_Blitz.Domain.Abstracts
 {
     public abstract class Location
@@ -5,6 +7,10 @@ namespace Beast_Blitz.Domain.Abstracts
         // DATA
         public int LocationID { get; set; }
         // Properties
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50)]
+        [MinLength(1)]
+        [StringLength(50, ErrorMessage = "Name must be between 1 and 50 characters", MinimumLength = 1)]
         public string Name { get; set; }
 
         // Constructor
