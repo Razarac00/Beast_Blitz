77ec59ab389973f99f65fb4cd13a73f17d7eec13
Razarac00 <orji.john@outlook.com>
2019-09-16 12:02:01 -0500

    Added error messages

--- Beast_Blitz.Domain/Abstracts/User.cs
+++ Beast_Blitz.Domain/Abstracts/User.cs
@@ -7,12 +7,14 @@ namespace Beast_Blitz.Domain.Abstracts
         // DATA
         public int UserID { get; set; }
         // Properties
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
         [MaxLength(100)]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
         [MaxLength(25)]
+        [MinLength(5)]
+        [StringLength(25, ErrorMessage = "Username must be between 5 and 25 characters", MinimumLength = 5)]
         public string Username { get; set; }
         
         [Required]
