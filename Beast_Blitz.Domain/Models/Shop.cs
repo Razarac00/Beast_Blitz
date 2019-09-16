d56d8dacca97f877b7dc32d8b633688d7eb4d123
Razarac00 <orji.john@outlook.com>
2019-09-16 16:00:37 -0500

    refactored inventory to depend on shopitems

--- Beast_Blitz.Domain/Models/Shop.cs
+++ Beast_Blitz.Domain/Models/Shop.cs
@@ -1,17 +1,20 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using Beast_Blitz.Domain.Abstracts;
 
 namespace Beast_Blitz.Domain.Models
 {
     public class Shop : Location
     {
+        // Backing Field
+        private List<Item> inventory = new List<Item>();
         // Properties
-        public List<Item> Inventory { get; set; }
+        protected List<Item> Inventory { get => buildInventory(); set => inventory = resetInventory(value); }
+        public List<ShopItems> ShopItems { get; set; }
 
         // Constructors
         public Shop(string name) : base(name)
         {
-          Inventory = new List<Item>();
         }
 
         public Shop(string name, List<Item> inventory) : base(name)
@@ -21,15 +24,40 @@ namespace Beast_Blitz.Domain.Models
 
         public Shop()
         {
-          Inventory = new List<Item>();
         }
 
         // Methods
+        private List<Item> buildInventory()
+        {
+            inventory = new List<Item>();
+
+            foreach (var shopItem in ShopItems)
+            {
+                inventory.Add(shopItem.Item);
+            }
+            return inventory;
+        }
+
+        private List<Item> resetInventory(List<Item> value)
+        {
+            ShopItems = new List<ShopItems>();
+
+            foreach (var item in value)
+            {
+                var si = new ShopItems();
+                si.Item = item;
+                si.LocationID = this.LocationID;
+                si.Shop = this;
+                ShopItems.Add(si);
+            }
+
+            return value;
+        }
         
         // Return true if buy was successful
         public bool Buy(Player player, Item item)
         {
-          if (player.Coins >= item.SellCost())
+          if (Inventory.Contains(item) && player.Coins >= item.SellCost())
           {
             player.Coins = player.Coins - item.SellCost();
             player.Inventory.Add(item);
