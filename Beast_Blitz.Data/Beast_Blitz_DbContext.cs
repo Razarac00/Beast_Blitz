9df18364dea717ef0387b2ff05e12ab69015e41c
Razarac00 <orji.john@outlook.com>
2019-09-15 19:26:54 -0500

    Initial working DbContext and relevant migrations

--- Beast_Blitz.Data/Beast_Blitz_DbContext.cs
+++ Beast_Blitz.Data/Beast_Blitz_DbContext.cs
@@ -14,19 +14,19 @@ namespace Beast_Blitz.Data
         public DbSet<Accessory> Accessories { get; set; }
 
         // CONCRETE //
-        // public DbSet<Player> Players { get; set; }
-        // public DbSet<Admin> Admins { get; set; }
-        // public DbSet<Pet> Pets { get; set; }
-        // public DbSet<Enemy> Enemies { get; set; }
-        // public DbSet<Boss> Bosses { get; set; }
-        // public DbSet<Shop> Shops { get; set; }
-        // public DbSet<Home> Homes { get; set; }
-        // public DbSet<Battlefield> Battlefields { get; set; }
-        // public DbSet<Treat> Treats { get; set; }
-        // public DbSet<Food> Foods { get; set; }
-        // public DbSet<Potion> Potions { get; set; }
-        // public DbSet<Armor> Armors { get; set; }
-        // public DbSet<Hat> Hats { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+        public DbSet<Boss> Bosses { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Home> Homes { get; set; }
+        public DbSet<Battlefield> Battlefields { get; set; }
+        public DbSet<Treat> Treats { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Potion> Potions { get; set; }
+        public DbSet<Armor> Armors { get; set; }
+        public DbSet<Hat> Hats { get; set; }
         public DbSet<Species> Species { get; set; }
         public DbSet<Element> Elements { get; set; }
         public DbSet<BattleStats> BattleStats { get; set; }
@@ -44,7 +44,7 @@ namespace Beast_Blitz.Data
             builder.Entity<Player>().HasMany(p => p.Pets);
             builder.Entity<Player>().HasMany(p => p.Inventory);
 
-            builder.Entity<Pet>().HasOne(p => p.Species);
+            // builder.Entity<Pet>().HasOne(p => p.Species);
             builder.Entity<Pet>().HasOne(p => p.CareStats);
             builder.Entity<Pet>().HasOne(p => p.BattleStats);
 
