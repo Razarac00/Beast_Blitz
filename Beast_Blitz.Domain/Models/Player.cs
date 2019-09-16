ce5fcbe83e40b10ff4bbd48a50df76b99f69c1c2
Razarac00 <orji.john@outlook.com>
2019-09-16 16:01:39 -0500

    begun refactoring from inventory to useritems

--- Beast_Blitz.Domain/Models/Player.cs
+++ Beast_Blitz.Domain/Models/Player.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using Beast_Blitz.Domain.Abstracts;
 
 namespace Beast_Blitz.Domain.Models
@@ -7,7 +8,10 @@ namespace Beast_Blitz.Domain.Models
   {
     // Properties 
     public List<Pet> Pets { get; set; }
+
+    [NotMapped]
     public List<Item> Inventory { get; set; }
+    public List<UserItems> UserItems { get; set; }
     public int Coins { get; set; }
 
     // Constants
