64cfc80102f4a242945828b14a6aebfe2c530849
Razarac00 <orji.john@outlook.com>
2019-09-16 13:04:22 -0500

    added email check to user

--- Beast_Blitz.Domain/Abstracts/User.cs
+++ Beast_Blitz.Domain/Abstracts/User.cs
@@ -9,6 +9,7 @@ namespace Beast_Blitz.Domain.Abstracts
         // Properties
         [Required(ErrorMessage = "Email is required")]
         [MaxLength(100)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Username is required")]
