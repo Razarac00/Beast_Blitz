4696a6360314c20d72fb687d4c1b4e0a46f54035
Razarac00 <orji.john@outlook.com>
2019-09-16 20:20:45 -0500

    database update reflected in sqlserver

---
+++ Beast_Blitz.Data/Migrations/20190917011328_speciesnotrequired.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Beast_Blitz.Data.Migrations
+{
+    public partial class speciesnotrequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Monsters_Species_SpeciesID",
+                table: "Monsters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpeciesID",
+                table: "Monsters",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Items",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Monsters_Species_SpeciesID",
+                table: "Monsters",
+                column: "SpeciesID",
+                principalTable: "Species",
+                principalColumn: "SpeciesID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Monsters_Species_SpeciesID",
+                table: "Monsters");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpeciesID",
+                table: "Monsters",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Monsters_Species_SpeciesID",
+                table: "Monsters",
+                column: "SpeciesID",
+                principalTable: "Species",
+                principalColumn: "SpeciesID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
